2300c6d8f3025e4e4bb4825501ec0b3578c6ffbc
rihi52 <rileyhire7@gmail.com>
2025-07-28 06:49:51 -0400

    enabled exp all. modified starter choices and torchic line stats Modified roxanne and brawly_1 parties evo items and reuse tms Made rare candies infinite Add wild areas to all towns. Modify Watson_1 party Add map for new petalburg woods area. Add encounters to dewford grass modified encounters in littleroot, 101, oldale, 102, 103, petalburg, 104, and started rustboro.

--- src/data/items.h
+++ src/data/items.h
@@ -1921,7 +1921,8 @@ const struct Item gItemsInfo[] =
             "Raises the level\n"
             "of a Pokémon by\n"
             "one."),
-        .pocket = POCKET_ITEMS,
+        .importance = 1,
+        .pocket = POCKET_KEY_ITEMS /*POCKET_ITEMS*/,
         .type = ITEM_USE_PARTY_MENU,
         .fieldUseFunc = ItemUseOutOfBattle_RareCandy,
         .effect = gItemEffect_RareCandy,
